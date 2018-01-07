f22d5689f6b7ecdf475e9306bfe7e2d7ca3297fe
Marco Vazquez <marcopvazquez@Marcos-MacBook-Pro.local>
2018-01-07 03:48:41 -0500

    Update info and score after the game ends; deliver message to user; save score info in local storage

--- public/js/game.js
+++ public/js/game.js
@@ -5,13 +5,39 @@
 
 	var answerList = [];
 	var gameLetter;
+	var round = null;
 
 	function endGame() {
-		var message = 'Congrats! You scored ' + answerList.length + ' out of 12!';
 		var gameMessageNode = document.getElementById('game-message');
+		var nextRoundHref = '/game/';
+		var nextRoundLink = document.getElementById('next-round-link');
+		var roundScoreNode = document.getElementById('round-score');
+		var totalScoreNode = document.getElementById('total-score');
+		var totalScore = answerList.length;
+
+		if (typeof Storage !== undefined) {
+			var pastScore = sessionStorage.getItem('score');
+			totalScore += Number(pastScore);
+			sessionStorage.setItem('score', totalScore);
+		}
+
+		if (round < 3) {
+			nextRoundHref += (round + 1);
+		} else {
+			var nextChildButton = document.getElementById('next-round-button');
+
+			nextChildButton.textContent = "New Game";
+			nextRoundHref += 1;
+			if (typeof Storage !== undefined) {
+				sessionStorage.removeItem('score');
+			}
+		}
 
 		gameMessageNode.classList = 'active';
-		gameMessageNode.textContent = message;
+		nextRoundLink.setAttribute('href', nextRoundHref);
+
+		roundScoreNode.textContent = answerList.length;
+		totalScoreNode.textContent = totalScore;
 	}
 
 	function markInputBorder(el, isValid) {
@@ -74,9 +100,22 @@
 
 	document.addEventListener("DOMContentLoaded", function() {
 		var categoryInputs = document.getElementsByClassName('category-input');
+		var gameScore = document.getElementById('game-score');
 		var letterContainer = document.getElementById('game-letter');
 		var rollButton = document.getElementById('roll-die-button');
 		var timerButton = document.getElementById('timer-button');
+
+		round = Number(document.getElementById('game-round').dataset.round);
+
+		if (typeof Storage !== undefined) {
+			var score = sessionStorage.getItem('score') || '0';
+
+			gameScore.textContent = score;
+		} else {
+			document.getElementById('game-score-container').remove();
+
+			console.error('Sorry, local web storage is not supported on your browser!');
+		}
 
 		for (var i = 0; i < categoryInputs.length; i++) {
 			categoryInputs[i].addEventListener('keydown', function(event) {
@@ -101,13 +140,13 @@
 
 			var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 			var randomNumber = Math.floor(Math.random() * alphabet.length);
-			var gameDetailsContainer = document.getElementById('game-details-container');
+			var gameDetailsRight = document.getElementById('game-details-container-right');
 
 			gameLetter = alphabet.charAt(randomNumber);
 
 			var gameLetterMessage = 'Letter: ' + gameLetter.toUpperCase();
 
-			gameDetailsContainer.classList.add('active-game-letter');
+			gameDetailsRight.classList.add('active-game-letter');
 
 			letterContainer.appendChild(document.createTextNode(gameLetterMessage));
 			rollButton.disabled = true;
@@ -118,20 +157,19 @@
 			event.preventDefault();
 
 			var categoryContainer = document.getElementById('category-container');
-			var gameTimer = document.getElementById('game-timer');
 			var firstInput = document.getElementById('category-input-0');
+			var gameTimer = document.getElementById('game-timer');
 			var timerCount = 120;
 
 			categoryContainer.classList.remove('blur-text');
 
 			toggleInputs(categoryInputs, false);
+			timerButton.disabled = true;
 
 			var timer = setInterval(function() {
-				var gameTimerContainer = document.getElementById('game-timer-container');
 				timerCount -= 1;
 
 				gameTimer.textContent = timerCount;
-				timerButton.disabled = true;
 
 				if (timerCount === 0) {
 					clearInterval(timer);
